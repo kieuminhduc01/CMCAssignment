75d579c7143bfc6f4a34b7388af6311fdb98498c
MinhDuc <kieuminhduc01@gmail.com>
2021-09-23 10:15:22 +0700

    Add GUIDE document

---
+++ MemberManagement/MemberService/Validator/UpdateMemberValidator.cs
@@ -0,0 +1,18 @@
+﻿using Common.Validator;
+using FluentValidation;
+using MemberService.Dtos;
+
+namespace MemberService.Validator
+{
+    public class UpdateMemberValidator: AbstractValidator<MemberUpdateVM>
+    {
+        public UpdateMemberValidator()
+        {
+            RuleFor(a => a.Name).NotNull().NotEmpty();
+            RuleFor(a => a.MobileNumber).NotNull().NotEmpty();
+            RuleFor(a => a.EmailOptIn).NotNull().NotEmpty().Must(a=>CommonValidator.EmailValidate(a));
+            RuleFor(a => a.Gender).NotNull().NotEmpty();
+            RuleFor(a => a.Dob).NotNull().NotEmpty();
+        }
+    }
+}
